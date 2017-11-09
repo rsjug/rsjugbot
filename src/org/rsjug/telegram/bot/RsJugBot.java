ff25e7d329028d17e85edb32c9b6e7cf83271ed7
Daniel Wildt <dwildt@gmail.com>
2017-11-09 00:13:01 -0200

    adding tdc support

--- src/org/rsjug/telegram/bot/RsJugBot.java
+++ src/org/rsjug/telegram/bot/RsJugBot.java
@@ -2,40 +2,66 @@ package org.rsjug.telegram.bot;
 
 public class RsJugBot {
 
+	public static final String PARECEQUE_MESSAGE = "Parece que você pediu ";
 	//Commands
 	public static final String COMMAND_SITE = "site";
 	public static final String COMMAND_FB = "fb";
 	public static final String COMMAND_TWITTER = "twitter";
+	public static final String COMMAND_TDC = "tdc";
 	
 	//Defaults
-	private static final String RSJUG_WEBSITE = "http://www.rsjug.org";
+	public static final String RSJUG_WEBSITE = "http://www.rsjug.org";
+	public static final String RSJUG_TWITTER = "http://twitter.com/rsjug";
+	public static final String RSJUG_FB      = "http://fb.com/groups/rsjug";
+	public static final String RSJUG_TDC     = "http://www.thedevelopersconference.com.br/tdc/2017/portoalegre/trilha-java";
+	
 
-	public String getWebSite() {
-		return RSJUG_WEBSITE;
-	}
+	public static final String START_MESSAGE = "Opções para teste: \n - site \n - fb \n - twitter \n - tdc";
+	public static final String NO_COMMAND_AVAILABLE = "Comando não reconhecido. \nTem alguma sugestão?\n";
 
 	public String execute(String cmd) {
 		
 		if(cmd!= null)
 			cmd = cmd.toLowerCase().trim();
 
-		if (cmd.contains("/start")) {
-			return "Opções: \n site \n fb \n twitter"; 
+		if (cmd.equals("/start")) {
+			return START_MESSAGE; 
 		}
 		
 		if (cmd.contains(COMMAND_SITE)) {
 			return getWebSite();
 		}
 		
+		if (cmd.contains(COMMAND_TDC)) {
+			return getTdc();
+		}
+
 		if (cmd.contains(COMMAND_FB)) {
-			return "http://fb.com/groups/rsjug";
+			return getFb();
 		}
 
 		if (cmd.contains(COMMAND_TWITTER)) {
-			return "http://twitter.com/rsjug";
+			if(cmd.equals(COMMAND_TWITTER)) return getTwitter();
+			return PARECEQUE_MESSAGE + COMMAND_TWITTER + " " + getTwitter();   
 		}
 
-		return null;
+		return NO_COMMAND_AVAILABLE+START_MESSAGE;
+	}
+
+	public String getTwitter() {
+		return RSJUG_TWITTER;
+	}
+
+	public String getFb() {
+		return RSJUG_FB;
+	}
+
+	public String getWebSite() {
+		return RSJUG_WEBSITE;
+	}
+
+	public String getTdc() {
+		return RSJUG_TDC;
 	}
 
 }
